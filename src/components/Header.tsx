5238241a277999ec9611604601731d1625135214
Dmitry Soldatenko <zeirison@gmail.com>
2022-01-10 00:35:19 +0200

    Create initial header

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import React, { useCallback } from "react";
+import { FlexProps } from "@chakra-ui/layout/dist/declarations/src/flex";
+import { Avatar, Box, Flex, Heading, useDisclosure } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import BottomArrowIcon from "../icons/BottomArrowIcon";
+
+const Header: React.FunctionComponent<FlexProps> = props => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const onClickMenu = useCallback(
+    () => (isOpen ? onClose() : onOpen()),
+    [isOpen, onClose, onOpen],
+  );
+
+  return (
+    <Flex
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      py="1.125rem"
+      px={{ base: "1.5rem", sm: 0 }}
+      {...props}
+    >
+      <Flex align="center">
+        <Heading textStyle="siteLogo">Smart Strategy</Heading>
+      </Flex>
+
+      <Box display={{ base: "block", sm: "none" }} onClick={onClickMenu}>
+        <HamburgerIcon color="white" width="1.5rem" height="1.5rem" />
+      </Box>
+
+      <Box display={{ base: "none", sm: "block" }}>
+        <Flex alignItems="center">
+          <Avatar bg="teal.500" mr="0.75rem" height="2.75rem" width="2.75rem" />
+          <BottomArrowIcon />
+        </Flex>
+      </Box>
+    </Flex>
+  );
+};
+
+export default React.memo(Header);
